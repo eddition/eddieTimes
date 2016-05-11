7f95d085abb04c386290e5d231b82cfd401b9ec0
Shin <eshn@USNYW7WVRENMAC2-5.local>
2016-05-11 12:58:25 -0400

    extracted factory and added style

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-angular.module('eddieTimes', [])
-.controller('mainController', ['$scope', function($scope){
+//MODULE
+var eddieTimesApp = angular.module('eddieTimes', ['ui.router']);
+
+//ROUTER CONFIG
+eddieTimesApp.config([
+	'$stateProvider', 
+	'$urlRouterProvider', 
+	function($stateProvider, $urlRouterProvider){
+
+		$stateProvider.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'mainController'
+		});
+
+		$urlRouterProvider.otherwise('home');
+
+	}
+]);
+
+//CONTROLLER
+eddieTimesApp.controller('mainController', [
+	 '$scope',
+	 'postsFactory' ,
+	function($scope, postsFactory){
 
 	$scope.test='Eddie Times';
 
-	$scope.posts = [
+	$scope.posts = postsFactory.posts;
+
+	[
 		{title: 'post 1', upvote: 3}
 		, {title: 'post 2', upvote: 8}
 		, {title: 'post 3', upvote: 9}
@@ -16,21 +41,32 @@ angular.module('eddieTimes', [])
 	//Add Post
 	$scope.addPost = function(){
 		
-		if ($scope.title === ''){
+		if ( !$scope.title||$scope.title === '' ){
 			return ;
 		}
 
 		$scope.posts.push({
-			title: $scope.title,
-			upvote: 0
+			title: $scope.title
+			, link: $scope.link
+			, upvote: 0
 		});
 		
 		$scope.title = '';
+		$scope.link = '';
 	};
 
 	//Vote Up
 	$scope.incrementUpvote = function(post){
 		post.upvote += 1;
 	};
+
+}]);
+
+eddieTimesApp.factory('postsFactory',[function(){
+	var postsObj = {
+		posts: []
+	}
+
+	return postsObj;
 
 }]);
